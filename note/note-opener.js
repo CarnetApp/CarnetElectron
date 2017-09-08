440f202c5af5e9280236d328666cd846ea90f70e
Phie <phie@phie.ovh>
2017-09-08 04:43:42 +0200

    changing save policy

--- note/note-opener.js
+++ note/note-opener.js
@@ -2,59 +2,61 @@ var JSZip = require('jszip');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 
-var NoteOpener = function(note){
-    this.note = note;
+var NoteOpener = function (note) {
+  this.note = note;
 }
 
-NoteOpener.prototype.getMainTextAndMetadata = function(callback){
+NoteOpener.prototype.getMainTextAndMetadata = function (callback) {
   var opener = this;
-    this.getFullHTML(function (data, zip) {
-      opener.getMetadataString(zip, function(metadata){
-        var tempElement =  document.createElement("div");
-        tempElement.innerHTML = data;
-        callback(tempElement.innerText, metadata!=undefined?JSON.parse(metadata):undefined)
-      })
-       
-    });
+  this.getFullHTML(function (data, zip) {
+    opener.getMetadataString(zip, function (metadata) {
+      var tempElement = document.createElement("div");
+      tempElement.innerHTML = data;
+      callback(tempElement.innerText, metadata != undefined ? JSON.parse(metadata) : undefined)
+    })
+
+  });
 }
 
-NoteOpener.prototype.getMetadataString = function(zip, callback){
-  if(zip.file("metadata.json")!=null)
+NoteOpener.prototype.getMetadataString = function (zip, callback) {
+  if (zip.file("metadata.json") != null)
     zip.file("metadata.json").async("string").then(callback)
-  else{
+  else {
     callback(undefined)
   }
 }
 
-NoteOpener.prototype.getFullHTML = function(callback){
-    fs.readFile(this.note.path,  function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        JSZip.loadAsync(data).then(function (zip) {
-            zip.file("index.html").async("string").then(function(content){
-              callback(content, zip)
-            })
-        });
-      }); 
+NoteOpener.prototype.getFullHTML = function (callback) {
+  fs.readFile(this.note.path, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    JSZip.loadAsync(data).then(function (zip) {
+      zip.file("index.html").async("string").then(function (content) {
+        callback(content, zip)
+      })
+    });
+  });
 }
 
-NoteOpener.prototype.extractTo = function(path, callback){
+NoteOpener.prototype.extractTo = function (path, callback) {
   var opener = this;
-    fs.readFile(this.note.path, function(err, data){
-        if (!err){
-          var extractor = new Extractor(data, path, opener, callback)
-          extractor.start();
-        }
-      });
+  console.log("extractTo")
+  fs.readFile(this.note.path, function (err, data) {
+    console.log("read " + err)
+    if (!err) {
+      var extractor = new Extractor(data, path, opener, callback)
+      extractor.start();
+    } else callback(true)
+  });
 }
 
-NoteOpener.prototype.compressFrom = function(path, callback){
-  var comp = new Compressor(path,this.note.path,callback);
+NoteOpener.prototype.compressFrom = function (path, callback) {
+  var comp = new Compressor(path, this.note.path, callback);
   comp.start();
 }
 
-var Extractor = function(data, dest, opener, callback){
+var Extractor = function (data, dest, opener, callback) {
   this.data = data;
   this.currentFile = 0;
   this.path = dest;
@@ -62,69 +64,70 @@ var Extractor = function(data, dest, opener, callback){
   this.opener = opener;
 }
 
-Extractor.prototype.start = function(){
+Extractor.prototype.start = function () {
   this.zip = new JSZip();
   var extractor = this;
-  this.zip.loadAsync(this.data).then(function(contents){
+  this.zip.loadAsync(this.data).then(function (contents) {
     extractor.files = Object.keys(contents.files);
-      extractor.fullExtract()
+    extractor.fullExtract()
   });
 }
 
-Extractor.prototype.fullExtract = function(){
-  console.log("fullExtract = "+this.files.length)
-  
-  if(this.currentFile >= this.files.length){
-    console.log("size = "+this.files.length)
+Extractor.prototype.fullExtract = function () {
+  console.log("fullExtract = " + this.files.length)
+
+  if (this.currentFile >= this.files.length) {
+    console.log("size = " + this.files.length)
     this.callback()
     return;
   }
   var filename = this.files[this.currentFile]
   var extractor = this;
-  console.log("extract  = "+filename)
+  console.log("extract  = " + filename)
   var file = this.zip.file(filename);
-  if(file != null){
-    file.async('nodebuffer').then(function(content) {
-    
-    
-    if(content!=""){
-      
-    var dest = extractor.path + filename;              
-    mkdirp.sync(getParentFolderFromPath(dest));
-    fs.writeFileSync(dest, content);
-    if(filename == "metadata.json"){
-      extractor.opener.note.metadata = JSON.parse(content);
-    }
-   
+  if (file != null) {
+    file.async('nodebuffer').then(function (content) {
+
+
+      if (content != "") {
+
+        var dest = extractor.path + filename;
+        mkdirp.sync(getParentFolderFromPath(dest));
+        fs.writeFileSync(dest, content);
+        if (filename == "metadata.json") {
+          extractor.opener.note.metadata = JSON.parse(content);
+        }
+
+      }
+      extractor.currentFile++;
+      extractor.fullExtract();
+    });
   }
-  extractor.currentFile ++;
-  extractor.fullExtract();
-}); 
-  }
-  else{
-    extractor.currentFile ++;
+  else {
+    extractor.currentFile++;
     extractor.fullExtract();
   }
 }
 
 
-var Compressor = function(source, dest, callback){
+var Compressor = function (source, dest, callback) {
   this.source = source;
   this.currentFile = 0;
   this.path = dest;
   this.callback = callback;
 }
 
-Compressor.prototype.start = function(){
+Compressor.prototype.start = function () {
   var fs = require('fs');
   var archiver = require('archiver');
   console.log("start")
   var archive = archiver.create('zip', {});
   var output = fs.createWriteStream(this.path);
   archive.pipe(output);
-  
+
   archive
-    .directory(__dirname,false )
+    .directory(__dirname, false)
     .finalize();
+  this.callback()
 }
 
--- reader/reader.js
+++ reader/reader.js
@@ -1,95 +1,108 @@
-var Writer = function(note, elem){
+var Writer = function (note, elem) {
     this.note = note;
     this.elem = elem;
     this.noteOpener = new NoteOpener(note);
-    
+    this.seriesTaskExecutor = new SeriesTaskExecutor();
+    this.saveNoteTask = new SaveNoteTask(this)
+
 }
 
 
-Writer.prototype.extractNote = function (){
-        var writer = this;
-        writer.noteOpener.extractTo("tmp/", function(){
-            console.log("done")
-            var fs = require('fs');            
+Writer.prototype.extractNote = function () {
+    var writer = this;
+    console.log("extractNote")
+    writer.noteOpener.extractTo("tmp/", function (noSuchFile) {
+        console.log("done")
+        if (!noSuchFile) {
+            var fs = require('fs');
             fs.readFile('tmp/index.html', function read(err, data) {
                 if (err) {
                     throw err;
                 }
-                
+
                 content = data;
                 console.log(data)
                 writer.fillWriter(content)
             });
-            //copying reader.html
-        })
+        }
+        else{
+            writer.fillWriter(undefined)
+        }
+        /*fs.readFile('tmp/metadata.json', function read(err, data) {
+            if (err) {
+                throw err;
+            }
+            
+            content = data;
+            console.log(data)
+            this.note.metadata = JSON.parse(content)
+        });*/
+        //copying reader.html
+    })
 }
 
-Writer.prototype.fillWriter = function (extractedHTML){
-    this.oEditor.innerHTML=extractedHTML;
-	this.oDoc = document.getElementById("text");
+Writer.prototype.fillWriter = function (extractedHTML) {
+    if (extractedHTML != undefined)
+        this.oEditor.innerHTML = extractedHTML;
+    this.oDoc = document.getElementById("text");
     this.oFloating = document.getElementById("floating");
-    var writer=  this
-    this.oDoc.addEventListener("input", function() {
-        var fs = require('fs');    
-        fs.unlink(__dirname+"/reader.html",function(){
-            fs.writeFile(__dirname+'/index.html', writer.oEditor.innerHTML, function (err) {
-                if (err) return console.log(err);
-                writer.note.metadata.last_modification_date = Date.now();
-                fs.writeFile(__dirname+'/metadata.json', JSON.stringify(writer.note.metadata), function (err) {
-                    if (err) return console.log(err);
-                    writer.oEditor.innerHTML
-                    writer.noteOpener.compressFrom(__dirname,function(){})
-                  });
-    
-              });
-            
-            
-        }) 
+    var writer = this
+    this.oDoc.addEventListener("input", function () {
         
+        writer.seriesTaskExecutor.addTask(writer.saveNoteTask.saveTxt)
     }, false);
     this.sDefTxt = this.oDoc.innerHTML;
-  /*simple initialization*/
-	this.oDoc.focus();
+    /*simple initialization*/
+    this.oDoc.focus();
     resetScreenHeight();
-     
+
     $("#editor").webkitimageresize().webkittableresize().webkittdresize();
-    
+
 }
 
-Writer.prototype.formatDoc = function(sCmd, sValue) {
-	oEditor.focus();
-    if (validateMode()) { document.execCommand(sCmd, false, sValue); oEditor.focus(); }
+Writer.prototype.formatDoc = function (sCmd, sValue) {
+    this.oEditor.focus();
+    if (validateMode()) { document.execCommand(sCmd, false, sValue); this.oEditor.focus(); }
 }
 
-Writer.prototype.init = function(){
+
+
+Writer.prototype.init = function () {
 
     this.statsDialog = this.elem.querySelector('#statsdialog');
     this.showDialogButton = this.elem.querySelector('#show-dialog');
-    if (! this.statsDialog.showModal) {
-    //  dialogPolyfill.registerDialog(this.statsDialog);
+    if (!this.statsDialog.showModal) {
+        //  dialogPolyfill.registerDialog(this.statsDialog);
     }
-   
-    this.statsDialog.querySelector('.ok').addEventListener('click', function() {
-		this.statsDialog.close();
-      
+
+    this.statsDialog.querySelector('.ok').addEventListener('click', function () {
+        this.statsDialog.close();
+
     });
 
     this.oEditor = document.getElementById("editor");
     this.backArrow = document.getElementById("back-arrow");
-    this.backArrow.addEventListener("click", function(){
-        var {ipcRenderer, remote} = require('electron');  
+    this.backArrow.addEventListener("click", function () {
+        var { ipcRenderer, remote } = require('electron');
         var main = remote.require("./main.js");
         var win = remote.getCurrentWindow();
         main.displayMainWindow(win.getSize(), win.getPosition());
         win.close()
     });
-   // $("#editor").webkitimageresize().webkittableresize().webkittdresize();
+    this.toolbarManager = new ToolbarManager()
+    this.toolbarManager.addToolbar(document.getElementById("format-toolbar"))
+    var toolbarManager = this.toolbarManager
+    document.getElementById("format-button").addEventListener("click", function () {
+        toolbarManager.toggleToolbar(document.getElementById("format-toolbar"))
+    });
+
+    // $("#editor").webkitimageresize().webkittableresize().webkittdresize();
 }
-Writer.prototype.displayCountDialog = function(){
-    if(window.getSelection().toString().length==0){
-        nouveauDiv = oDoc;			
+Writer.prototype.displayCountDialog = function () {
+    if (window.getSelection().toString().length == 0) {
+        nouveauDiv = oDoc;
     }
-    else{
+    else {
         nouveauDiv = document.createElement("div");
         nouveauDiv.innerHTML = window.getSelection();
     }
@@ -100,4 +113,92 @@ Writer.prototype.displayCountDialog = function(){
         mStatsDialog.showModal();
     });
 
+}
+
+
+
+var ToolbarManager = function(){
+    this.toolbars = [];
+}
+ToolbarManager.prototype.addToolbar = function(elem){
+    this.toolbars.push(elem)
+    $(elem).hide()
+}
+
+ToolbarManager.prototype.toggleToolbar = function(elem){
+    for(let toolbar of this.toolbars){
+        if(toolbar != elem)
+            $(toolbar).hide()
+    }
+    $(elem).show()
+    
+    resetScreenHeight()
+}
+
+
+var SeriesTaskExecutor = function(){
+    this.task = []
+    this.isExecuting = false
+}
+
+SeriesTaskExecutor.prototype.addTask = function(task){
+    this.task.push(task)
+    console.log("push "+this.isExecuting)
+    
+    if(!this.isExecuting){
+        this.execNext()
+    }
+    
+}
+
+SeriesTaskExecutor.prototype.execNext = function(){
+    this.isExecuting = true
+    console.log("exec next ")
+    if(this.task == undefined)
+        this.task = []
+    if(this.task.length == 0){
+        this.isExecuting = false;
+        return;
+    }
+     var executor = this;  
+    this.task.shift()(function(){
+        executor.execNext()
+    })
+    console.log("this.task length "+this.task.length)
+    
+}
+
+var SaveNoteTask = function(writer){
+    this.writer=writer;
+
+}
+
+SaveNoteTask.prototype.saveTxt = function(onEnd){
+    var fs = require('fs');
+    console.log("saving")
+    fs.unlink(__dirname + "/reader.html", function () {
+        fs.writeFile(__dirname + '/index.html', this.writer.oEditor.innerHTML, function (err) {
+            if (err){
+                onEnd()
+                return console.log(err);
+            } 
+            this.writer.note.metadata.last_modification_date = Date.now();
+            fs.writeFile(__dirname + '/metadata.json', JSON.stringify(this.writer.note.metadata), function (err) {
+                if (err){
+                    onEnd()
+                    return console.log(err);
+                } 
+                this.writer.oEditor.innerHTML
+                console.log("compress")
+                this.writer.noteOpener.compressFrom(__dirname, function () {
+                    console.log("compressed")
+                    
+                    onEnd()
+                 })
+            });
+
+        });
+
+
+    })
 }
