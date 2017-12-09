a0d1735a1ce1d2a17784886cde50db3b7f1fb511
Phoenamandre <phoenamandre@gmail.com>
2017-12-09 16:16:42 +0100

    icon

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function startMerging(){
 function createWindow() {
     startMerging();
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600, frame: false })
+    win = new BrowserWindow({ width: 800, height: 600, frame: false,icon: path.join(__dirname, 'assets/images/QuickDoc.png') })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
