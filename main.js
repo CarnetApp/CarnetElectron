7e7e1f7253bd0150c8b53545bca3bdbf2fc67fbd
Phoenamandre <phoenamandre@gmail.com>
2017-12-09 17:19:40 +0100

    bigger window + don't open dev tools

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function startMerging(){
 function createWindow() {
     startMerging();
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600, frame: false,icon: path.join(__dirname, 'assets/images/QuickDoc.png') })
+    win = new BrowserWindow({ width: 1030, height: 600, frame: false,icon: path.join(__dirname, 'assets/images/QuickDoc.png') })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
