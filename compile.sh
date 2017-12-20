9c5d3f7ba2b71d96f638048390695d07d0ded57e
Phoenamandre <phoenamandre@gmail.com>
2017-12-21 00:05:30 +0100

    moving compat lib

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ fi
 if [ "$1" == "android" ]; then
     echo "compiling for android"
     cp reader "$2"/app/src/main/assets/reader/ -R
-    cp compatibility "$2"/app/src/main/assets/reader/ -R
+    cp compatibility/android "$2"/app/src/main/assets/reader/compatibility -R
     cp compatibility.js "$2"/app/src/main/assets/reader/ -R
     cp browsers "$2"/app/src/main/assets/reader/ -R
     cp libs "$2"/app/src/main/assets/reader/ -R
