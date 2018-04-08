bbfd5b85a259fbe08becd2ca4931340e41d4c7cb
Phie <phie@phie.ovh>
2018-04-09 00:27:23 +0200

    quickdoc instead of quicknote

--- settings/settings_helper.js
+++ settings/settings_helper.js
@@ -12,7 +12,7 @@ SettingsHelper.prototype.getNotePath = function () {
         } = require('electron')
         if (app == undefined)
             app = remote.app
-        path = app.getPath('documents') + "/QuickNote";
+        path = app.getPath('documents') + "/QuickDoc";
         store.set("root_path", path);
     }
     require("mkdirp")(path)
