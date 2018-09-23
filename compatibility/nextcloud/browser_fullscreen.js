b384d3c0c4f276d575b0de0ff839df0a216e3ccc
Phie <phie@phie.ovh>
2018-09-23 16:46:26 +0200

    fix nc 14

--- compatibility/nextcloud/browser_fullscreen.js
+++ compatibility/nextcloud/browser_fullscreen.js
@@ -4,7 +4,13 @@ settings.parentNode.removeChild(settings)
 right2.appendChild(settings)
 const header = document.getElementById("header")
 header.parentNode.removeChild(header)
-document.getElementById("content-wrapper").style.paddingTop = "0px"
+var nc_content = document.getElementById("content-wrapper");
+if (nc_content == null)
+    nc_content = document.getElementById("content")
+nc_content.style.paddingTop = "0px"
+
 const ex = document.getElementById("expanddiv")
-ex.style.top = "55px";
+ex.style.top = "50px";
 ex.style.position = "absolute";
+ex.style.right = "0px";
+ex.style.background = "white"
