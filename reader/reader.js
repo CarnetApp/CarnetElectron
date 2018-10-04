e4622b2044682cfb99a8f2800b1aac8ef7806e86
Phie <phie@phie.ovh>
2018-10-04 16:48:48 +0200

    mistake : type is object, not function

--- reader/reader.js
+++ reader/reader.js
@@ -703,7 +703,7 @@ Writer.prototype.askToExit = function () {
 Writer.prototype.exit = function () {
     if(window.self !== window.top) //in iframe
         parent.postMessage("exit", "*")
-    else if(typeof app === "function") app.postMessage("exit", "*");
+    else if(typeof app === "object") app.postMessage("exit", "*");
 }
 
 Writer.prototype.copy = function () {
@@ -713,7 +713,7 @@ Writer.prototype.copy = function () {
 
 Writer.prototype.paste = function () {
     if(!document.execCommand('paste')){
-        if(typeof app === "function")
+        if(typeof app === "object")
             app.paste(); //for android app
         else
             document.execCommand('insertHTML', false, this.copied)
