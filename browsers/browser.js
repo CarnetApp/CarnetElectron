d080d5e2fa1720d03e123eaa060b1e5476335fc7
Phie <phie@phie.ovh>
2018-10-21 15:49:54 +0200

    implementing size + exit  + minus button

--- browsers/browser.js
+++ browsers/browser.js
@@ -562,27 +562,7 @@ function isFullScreen() {
         document.webkitFullscreenElement ||
         document.msFullscreenElement
 }
-document.getElementById("size-button").onclick = function () {
-    if (!isFullScreen()) {
-        const docElm = document.getElementById("content");
-        if (docElm.requestFullscreen) {
-            docElm.requestFullscreen();
-        } else if (docElm.mozRequestFullScreen) {
-            docElm.mozRequestFullScreen();
-        } else if (docElm.webkitRequestFullScreen) {
-            docElm.webkitRequestFullScreen();
-        }
 
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitCancelFullScreen) {
-            document.webkitCancelFullScreen();
-        }
-    }
-}
 
 
 RequestBuilder.sRequestBuilder.get("/recentdb/merge", function (error, data) {
--- compatibility/compatibility-browser.js
+++ compatibility/compatibility-browser.js
@@ -8,7 +8,44 @@ class BrowserCompatibility extends Compatibility {
                 right.removeChild(document.getElementById("minus-button"))
                 right.removeChild(document.getElementById("close-button"))
                 document.getElementById("settings-button").href = "./settings"
+                document.getElementById("size-button").onclick = function () {
+                    if (!isFullScreen()) {
+                        const docElm = document.getElementById("content");
+                        if (docElm.requestFullscreen) {
+                            docElm.requestFullscreen();
+                        } else if (docElm.mozRequestFullScreen) {
+                            docElm.mozRequestFullScreen();
+                        } else if (docElm.webkitRequestFullScreen) {
+                            docElm.webkitRequestFullScreen();
+                        }
 
+                    } else {
+                        if (document.exitFullscreen) {
+                            document.exitFullscreen();
+                        } else if (document.mozCancelFullScreen) {
+                            document.mozCancelFullScreen();
+                        } else if (document.webkitCancelFullScreen) {
+                            document.webkitCancelFullScreen();
+                        }
+                    }
+                }
+
+            } else {
+                const {
+                    remote
+                } = require('electron');
+                document.getElementById("minus-button").onclick = function () {
+                    remote.BrowserWindow.getFocusedWindow().minimize();
+                }
+                document.getElementById("size-button").onclick = function () {
+                    if (remote.BrowserWindow.getFocusedWindow().isMaximized())
+                        remote.BrowserWindow.getFocusedWindow().unmaximize();
+                    else
+                        remote.BrowserWindow.getFocusedWindow().maximize();
+                }
+                document.getElementById("close-button").onclick = function () {
+                    remote.app.exit(0);
+                }
             }
         });
 
