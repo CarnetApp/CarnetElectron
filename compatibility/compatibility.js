9b7b6b4bfd1b4986d689e2c9af77b09bba664088
Phie <phie@phie.ovh>
2019-04-17 16:23:48 +0200

    enabling russian, dutch, czech, and slovak languages

--- compatibility/compatibility.js
+++ compatibility/compatibility.js
@@ -25,7 +25,7 @@ class Compatibility {
     }
 
     loadLang(callback) {
-        var langs = ["en", "fr", "de"];
+        var langs = ["en", "fr", "de", "ru", "nl", "cs", "sk"];
         var toLoad = {}
 
         for (var lang of langs) {
