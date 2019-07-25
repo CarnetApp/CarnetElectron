5506252a948ee950067926331dfa248b252b07be
Phie <phie@phie.ovh>
2019-07-25 22:25:36 +0200

    compatibility for electron

--- compatibility/compatibility-browser.js
+++ compatibility/compatibility-browser.js
@@ -39,6 +39,18 @@ class BrowserCompatibility extends Compatibility {
                     remote,
                     ipcRenderer
                 } = require('electron');
+                $('head').append("<style>\
+                @media screen and (min-width: 1400px) {\
+				#loading-view {\
+					top: 20px;\
+					height: calc(100% - 40px);\
+				}\
+				#editor-container{\
+					height:calc(100% - 40px);\
+					padding-top:20px;\
+					padding-bottom:20px;\
+				}\
+			}</style>");
                 var syncButton = document.getElementById("sync-button");
                 syncButton.style.display = "inline"
                 ipcRenderer.on('sync-start', (event, arg) => {
