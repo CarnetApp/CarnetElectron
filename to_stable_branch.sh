fcb567a038885aab35f2916049b40acc8598a5d5
Phie <phie@phie.ovh>
2020-02-08 19:26:30 +0100

    update script

--- to_stable_branch.sh
+++ to_stable_branch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-
+version=$(sed -n -e 's/.*\"version\": \"\(.*\)\".*/\1/p' package.json)
+echo $version > version
 git branch -d stable
 git push origin --delete stable
 git checkout master
 git checkout -b stable
 git push origin stable
+git checkout -b stable-$version
+git push origin stable-$version
 git checkout master
