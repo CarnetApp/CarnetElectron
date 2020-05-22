a1ed213e5763ee655bead4c6f624c85dc1a8b18b
Phie <phie@phie.ovh>
2020-05-22 19:29:44 +0200

    fix again mac release script

--- release_dmg.sh
+++ release_dmg.sh
@@ -3,5 +3,5 @@ git checkout stable
 version=$(sed -n -e 's/.*\"version\": \"\(.*\)\".*/\1/p' package.json)
 yarn run distdmg
 scp dist/Carnet-${version}.dmg phie@qn.phie.ovh:/var/www/html/quicknote/binaries/desktop/daily/
-ssh phie@qn.phie.ovh 'cp /var/www/html/quicknote/binaries/desktop/daily/Carnet_${version}.dmg /var/www/html/quicknote/binaries/desktop/currentMac.dmg'
+ssh phie@qn.phie.ovh 'cp /var/www/html/quicknote/binaries/desktop/daily/Carnet-'${version}'.dmg /var/www/html/quicknote/binaries/desktop/currentMac.dmg'
 git checkout master
