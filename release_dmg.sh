4cb9edb61986b31aa358fec06ea5ce7572395c50
Phie <phie@phie.ovh>
2020-05-22 19:04:20 +0200

    error on dmg name

--- release_dmg.sh
+++ release_dmg.sh
@@ -2,6 +2,6 @@
 git checkout stable
 version=$(sed -n -e 's/.*\"version\": \"\(.*\)\".*/\1/p' package.json)
 yarn run distdmg
-scp dist/Carnet_${version}.dmg phie@qn.phie.ovh:/var/www/html/quicknote/binaries/desktop/daily/
+scp dist/Carnet-${version}.dmg phie@qn.phie.ovh:/var/www/html/quicknote/binaries/desktop/daily/
 ssh phie@qn.phie.ovh 'cp /var/www/html/quicknote/binaries/desktop/daily/Carnet_${version}.dmg /var/www/html/quicknote/binaries/desktop/currentMac.dmg'
 git checkout master
