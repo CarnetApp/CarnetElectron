6bc3b69823e2d4a7370fa07c3e8b976208d341e3
Phie <phie@phie.ovh>
2020-11-05 19:20:59 +0100

    put minified in another folder

--- minify.sh
+++ minify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+mkdir minified
 files=`sed -n -e 's/.*script.*src.*"\(.*\)\".*/\1/p' index.html | grep -v jquery | grep -v  /browser.js | grep -v search.js | grep -v browsers/browser.depedencies.min.js` 
 
-terser $files -o browsers/browser.depedencies.min.js
+terser $files -o minified/browser.depedencies.min.js
 sed  "s/startjs-->//g" index.html -i
 sed  "s/<\!--endjs//g" index.html -i
 sed  "s/<\!--startmin//g" index.html -i
@@ -9,7 +10,7 @@ sed  "s/endmin-->//g" index.html -i
 
 files=`sed -n -e 's/.*script.*src.*"\(.*\)\".*/\1/p' reader/reader.html | grep -v jquery.min.js | grep -v  /browser.js | grep -v reader/reader.min.js | sed "s/<\!ROOTPATH>//g"`
 
-terser $files -o reader/reader.min.js
+terser $files -o minified/reader.min.js
 sed  "s/startjs-->//g" reader/reader.html -i
 sed  "s/<\!--endjs//g" reader/reader.html -i
 sed  "s/<\!--startmin//g" reader/reader.html -i
