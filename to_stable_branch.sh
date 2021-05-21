78007590a7fca55fd381f91af94f52a4dc619a5c
Phie <phie@phie.ovh>
2021-05-21 21:28:36 +0200

    main is the new... main branch !

--- to_stable_branch.sh
+++ to_stable_branch.sh
@@ -7,13 +7,13 @@ git commit -m "update version number to "+$version
 git branch -D stable
 git push origin --delete stable
 git push framagit --delete stable
-git checkout master
+git checkout main
 git checkout -b stable
 git push origin stable
 git push framagit stable
 git checkout -b stable-$version
 git push origin stable-$version
 git push framagit stable-$version
-git checkout master
+git checkout main
 git push
 
