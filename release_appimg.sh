2838c834903d9a477025d8a81d898d83f67e9c3f
Alexandre Roux D'Anzi <alexandre.roux.danzi@lostpod.me>
2022-02-14 18:50:02 +0100

    fix... again.....

--- release_appimg.sh
+++ release_appimg.sh
@@ -6,8 +6,8 @@ version=$(sed -n -e 's/.*\"version\": \"\(.*\)\".*/\1/p' package.json)
 yarn run distapp
 scp version phie@qn.phie.ovh:${path}/binaries/desktop/daily/
 scp dist/Carnet-${version}.AppImage phie@qn.phie.ovh:${path}/binaries/desktop/daily/
-ssh phie@qn.phie.ovh 'cp "${path}"binaries/desktop/daily/Carnet-'${version}'.AppImage '${path}'/binaries/desktop/current64.AppImage'
-scp dist/Carnet-${version}-i386.AppImage phie@qn.phie.ovh:"${path}"binaries/desktop/daily/
-ssh phie@qn.phie.ovh 'cp "${path}"binaries/desktop/daily/Carnet-'${version}'-i386.AppImage '${path}'/binaries/desktop/current32.AppImage'
+ssh phie@qn.phie.ovh 'cp '${path}'/binaries/desktop/daily/Carnet-'${version}'.AppImage '${path}'/binaries/desktop/current64.AppImage'
+scp dist/Carnet-${version}-i386.AppImage phie@qn.phie.ovh:${path}/binaries/desktop/daily/
+ssh phie@qn.phie.ovh 'cp '${path}'/binaries/desktop/daily/Carnet-'${version}'-i386.AppImage '${path}'/binaries/desktop/current32.AppImage'
 
 git checkout main
